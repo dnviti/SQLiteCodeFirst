1465238da8e619652748013330dae276e88f26fe
Daniele Viti <dnviti@gmail.com>
2019-07-15 15:33:07 +0200

    Ciclomatic Corrections

--- SQLite.CodeFirst/Internal/Utility/MigrationFormatter.cs
+++ SQLite.CodeFirst/Internal/Utility/MigrationFormatter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.CodeDom.Compiler;
+using System.Collections.Generic;
 using System.Data.Entity.Core.Common;
 using System.Data.Entity.Infrastructure.Annotations;
 using System.Data.Entity.Migrations.Model;
@@ -10,11 +11,11 @@ namespace SQLite.CodeFirst.Utility
 {
     public static class MigrationFormatter
     {
-        const int _defaultStringMaxLength = 255;
-        const int _defaultNumericPrecision = 10;
-        const byte _defaultTimePrecision = 7;
-        const byte _defaultNumericScale = 0;
-        
+        private const int DefaultStringMaxLength = 255;
+        private const int DefaultNumericPrecision = 10;
+        private const byte DefaultTimePrecision = 7;
+        private const byte DefaultNumericScale = 0;
+
         /// <summary>
         /// Builds a column type
         /// </summary>
@@ -50,26 +51,19 @@ namespace SQLite.CodeFirst.Utility
             if (storeType.EndsWith(maxSuffix, StringComparison.Ordinal))
                 storeType = storeType.Substring(0, storeType.Length - maxSuffix.Length) + maxSuffix;
 
-            switch (originalStoreType.ToUpperInvariant())
-            {
-                case "DECIMAL":
-                case "NUMERIC":
-                    storeType += "(" + (column.Precision ?? _defaultNumericPrecision)
-                                     + ", " + (column.Scale ?? _defaultNumericScale) + ")";
-                    break;
-                case "DATETIME":
-                case "TIME":
-                    storeType += "(" + (column.Precision ?? _defaultTimePrecision) + ")";
-                    break;
-                case "BLOB":
-                case "VARCHAR2":
-                case "VARCHAR":
-                case "CHAR":
-                case "NVARCHAR":
-                case "NVARCHAR2":
-                    storeType += "(" + (column.MaxLength ?? _defaultStringMaxLength) + ")";
-                    break;
-            }
+            if (originalStoreType.ToUpperInvariant() == "DECIMAL" || originalStoreType.ToUpperInvariant() == "NUMERIC")
+                storeType += "(" + (column.Precision ?? DefaultNumericPrecision)
+                                 + ", " + (column.Scale ?? DefaultNumericScale) + ")";
+            else if (originalStoreType.ToUpperInvariant() == "DATETIME" ||
+                     originalStoreType.ToUpperInvariant() == "TIME")
+                storeType += "(" + (column.Precision ?? DefaultTimePrecision) + ")";
+            else if (originalStoreType.ToUpperInvariant() == "BLOB" ||
+                     originalStoreType.ToUpperInvariant() == "VARCHAR2" ||
+                     originalStoreType.ToUpperInvariant() == "VARCHAR" ||
+                     originalStoreType.ToUpperInvariant() == "CHAR" ||
+                     originalStoreType.ToUpperInvariant() == "NVARCHAR" ||
+                     originalStoreType.ToUpperInvariant() == "NVARCHAR2")
+                storeType += "(" + (column.MaxLength ?? DefaultStringMaxLength) + ")";
 
             return storeType;
         }
@@ -111,137 +105,273 @@ namespace SQLite.CodeFirst.Utility
             if (word == null)
                 return string.Empty;
 
-            switch (word.ToUpper(CultureInfo.InvariantCulture))
+            List<string> cultureList = new List<string>
             {
-                case "ABORT":
-                case "ACTION":
-                case "ADD":
-                case "AFTER":
-                case "ALL":
-                case "ALTER":
-                case "ANALYZE":
-                case "AND":
-                case "AS":
-                case "ASC":
-                case "ATTACH":
-                case "AUTOINCREMENT":
-                case "BEFORE":
-                case "BEGIN":
-                case "BETWEEN":
-                case "BY":
-                case "CASCADE":
-                case "CASE":
-                case "CAST":
-                case "CHECK":
-                case "COLLATE":
-                case "COLUMN":
-                case "COMMIT":
-                case "CONFLICT":
-                case "CONSTRAINT":
-                case "CREATE":
-                case "CROSS":
-                case "CURRENT_DATE":
-                case "CURRENT_TIME":
-                case "CURRENT_TIMESTAMP":
-                case "DATABASE":
-                case "DEFAULT":
-                case "DEFERRABLE":
-                case "DEFERRED":
-                case "DELETE":
-                case "DESC":
-                case "DETACH":
-                case "DISTINCT":
-                case "DROP":
-                case "EACH":
-                case "ELSE":
-                case "END":
-                case "ESCAPE":
-                case "EXCEPT":
-                case "EXCLUSIVE":
-                case "EXISTS":
-                case "EXPLAIN":
-                case "FAIL":
-                case "FOR":
-                case "FOREIGN":
-                case "FROM":
-                case "FULL":
-                case "GLOB":
-                case "GROUP":
-                case "HAVING":
-                case "IF":
-                case "IGNORE":
-                case "IMMEDIATE":
-                case "IN":
-                case "INDEX":
-                case "INDEXED":
-                case "INITIALLY":
-                case "INNER":
-                case "INSERT":
-                case "INSTEAD":
-                case "INTERSECT":
-                case "INTO":
-                case "IS":
-                case "ISNULL":
-                case "JOIN":
-                case "KEY":
-                case "LEFT":
-                case "LIKE":
-                case "LIMIT":
-                case "MATCH":
-                case "NATURAL":
-                case "NO":
-                case "NOT":
-                case "NOTNULL":
-                case "NULL":
-                case "OF":
-                case "OFFSET":
-                case "ON":
-                case "OR":
-                case "ORDER":
-                case "OUTER":
-                case "PLAN":
-                case "PRAGMA":
-                case "PRIMARY":
-                case "QUERY":
-                case "RAISE":
-                case "RECURSIVE":
-                case "REFERENCES":
-                case "REGEXP":
-                case "REINDEX":
-                case "RELEASE":
-                case "RENAME":
-                case "REPLACE":
-                case "RESTRICT":
-                case "RIGHT":
-                case "ROLLBACK":
-                case "ROW":
-                case "SAVEPOINT":
-                case "SELECT":
-                case "SET":
-                case "TABLE":
-                case "TEMP":
-                case "TEMPORARY":
-                case "THEN":
-                case "TO":
-                case "TRANSACTION":
-                case "TRIGGER":
-                case "UNION":
-                case "UNIQUE":
-                case "UPDATE":
-                case "USING":
-                case "VACUUM":
-                case "VALUES":
-                case "VIEW":
-                case "VIRTUAL":
-                case "WHEN":
-                case "WHERE":
-                case "WITH":
-                case "WITHOUT":
-                    return '"' + word + '"';
+                "ABORT",
+                "ACTION",
+                "ADD",
+                "AFTER",
+                "ALL",
+                "ALTER",
+                "ANALYZE",
+                "AND",
+                "AS",
+                "ASC",
+                "ATTACH",
+                "AUTOINCREMENT",
+                "BEFORE",
+                "BEGIN",
+                "BETWEEN",
+                "BY",
+                "CASCADE",
+                "CASE",
+                "CAST",
+                "CHECK",
+                "COLLATE",
+                "COLUMN",
+                "COMMIT",
+                "CONFLICT",
+                "CONSTRAINT",
+                "CREATE",
+                "CROSS",
+                "CURRENT_DATE",
+                "CURRENT_TIME",
+                "CURRENT_TIMESTAMP",
+                "DATABASE",
+                "DEFAULT",
+                "DEFERRABLE",
+                "DEFERRED",
+                "DELETE",
+                "DESC",
+                "DETACH",
+                "DISTINCT",
+                "DROP",
+                "EACH",
+                "ELSE",
+                "END",
+                "ESCAPE",
+                "EXCEPT",
+                "EXCLUSIVE",
+                "EXISTS",
+                "EXPLAIN",
+                "FAIL",
+                "FOR",
+                "FOREIGN",
+                "FROM",
+                "FULL",
+                "GLOB",
+                "GROUP",
+                "HAVING",
+                "IF",
+                "IGNORE",
+                "IMMEDIATE",
+                "IN",
+                "INDEX",
+                "INDEXED",
+                "INITIALLY",
+                "INNER",
+                "INSERT",
+                "INSTEAD",
+                "INTERSECT",
+                "INTO",
+                "IS",
+                "ISNULL",
+                "JOIN",
+                "KEY",
+                "LEFT",
+                "LIKE",
+                "LIMIT",
+                "MATCH",
+                "NATURAL",
+                "NO",
+                "NOT",
+                "NOTNULL",
+                "NULL",
+                "OF",
+                "OFFSET",
+                "ON",
+                "OR",
+                "ORDER",
+                "OUTER",
+                "PLAN",
+                "PRAGMA",
+                "PRIMARY",
+                "QUERY",
+                "RAISE",
+                "RECURSIVE",
+                "REFERENCES",
+                "REGEXP",
+                "REINDEX",
+                "RELEASE",
+                "RENAME",
+                "REPLACE",
+                "RESTRICT",
+                "RIGHT",
+                "ROLLBACK",
+                "ROW",
+                "SAVEPOINT",
+                "SELECT",
+                "SET",
+                "TABLE",
+                "TEMP",
+                "TEMPORARY",
+                "THEN",
+                "TO",
+                "TRANSACTION",
+                "TRIGGER",
+                "UNION",
+                "UNIQUE",
+                "UPDATE",
+                "USING",
+                "VACUUM",
+                "VALUES",
+                "VIEW",
+                "VIRTUAL",
+                "WHEN",
+                "WHERE",
+                "WITH",
+                "WITHOUT"
+            };
 
-                default:
-                    return word;
+            string elem = cultureList.Find(i => i == word.ToUpper(CultureInfo.InvariantCulture));
+
+            if (elem != null)
+            {
+                return '"' + word + '"';
             }
+            else { return word; }
+
+            //switch (word.ToUpper(CultureInfo.InvariantCulture))
+            //{
+            //    case "ABORT":
+            //    case "ACTION":
+            //    case "ADD":
+            //    case "AFTER":
+            //    case "ALL":
+            //    case "ALTER":
+            //    case "ANALYZE":
+            //    case "AND":
+            //    case "AS":
+            //    case "ASC":
+            //    case "ATTACH":
+            //    case "AUTOINCREMENT":
+            //    case "BEFORE":
+            //    case "BEGIN":
+            //    case "BETWEEN":
+            //    case "BY":
+            //    case "CASCADE":
+            //    case "CASE":
+            //    case "CAST":
+            //    case "CHECK":
+            //    case "COLLATE":
+            //    case "COLUMN":
+            //    case "COMMIT":
+            //    case "CONFLICT":
+            //    case "CONSTRAINT":
+            //    case "CREATE":
+            //    case "CROSS":
+            //    case "CURRENT_DATE":
+            //    case "CURRENT_TIME":
+            //    case "CURRENT_TIMESTAMP":
+            //    case "DATABASE":
+            //    case "DEFAULT":
+            //    case "DEFERRABLE":
+            //    case "DEFERRED":
+            //    case "DELETE":
+            //    case "DESC":
+            //    case "DETACH":
+            //    case "DISTINCT":
+            //    case "DROP":
+            //    case "EACH":
+            //    case "ELSE":
+            //    case "END":
+            //    case "ESCAPE":
+            //    case "EXCEPT":
+            //    case "EXCLUSIVE":
+            //    case "EXISTS":
+            //    case "EXPLAIN":
+            //    case "FAIL":
+            //    case "FOR":
+            //    case "FOREIGN":
+            //    case "FROM":
+            //    case "FULL":
+            //    case "GLOB":
+            //    case "GROUP":
+            //    case "HAVING":
+            //    case "IF":
+            //    case "IGNORE":
+            //    case "IMMEDIATE":
+            //    case "IN":
+            //    case "INDEX":
+            //    case "INDEXED":
+            //    case "INITIALLY":
+            //    case "INNER":
+            //    case "INSERT":
+            //    case "INSTEAD":
+            //    case "INTERSECT":
+            //    case "INTO":
+            //    case "IS":
+            //    case "ISNULL":
+            //    case "JOIN":
+            //    case "KEY":
+            //    case "LEFT":
+            //    case "LIKE":
+            //    case "LIMIT":
+            //    case "MATCH":
+            //    case "NATURAL":
+            //    case "NO":
+            //    case "NOT":
+            //    case "NOTNULL":
+            //    case "NULL":
+            //    case "OF":
+            //    case "OFFSET":
+            //    case "ON":
+            //    case "OR":
+            //    case "ORDER":
+            //    case "OUTER":
+            //    case "PLAN":
+            //    case "PRAGMA":
+            //    case "PRIMARY":
+            //    case "QUERY":
+            //    case "RAISE":
+            //    case "RECURSIVE":
+            //    case "REFERENCES":
+            //    case "REGEXP":
+            //    case "REINDEX":
+            //    case "RELEASE":
+            //    case "RENAME":
+            //    case "REPLACE":
+            //    case "RESTRICT":
+            //    case "RIGHT":
+            //    case "ROLLBACK":
+            //    case "ROW":
+            //    case "SAVEPOINT":
+            //    case "SELECT":
+            //    case "SET":
+            //    case "TABLE":
+            //    case "TEMP":
+            //    case "TEMPORARY":
+            //    case "THEN":
+            //    case "TO":
+            //    case "TRANSACTION":
+            //    case "TRIGGER":
+            //    case "UNION":
+            //    case "UNIQUE":
+            //    case "UPDATE":
+            //    case "USING":
+            //    case "VACUUM":
+            //    case "VALUES":
+            //    case "VIEW":
+            //    case "VIRTUAL":
+            //    case "WHEN":
+            //    case "WHERE":
+            //    case "WITH":
+            //    case "WITHOUT":
+            //        return '"' + word + '"';
+
+            //    default:
+            //        return word;
+            //}
         }
 
         public static string UniqueConflictText(AnnotationValues uniqueAnnotation)
@@ -250,13 +380,12 @@ namespace SQLite.CodeFirst.Utility
                 return string.Empty;
 
             var uniqueText = Convert.ToString(uniqueAnnotation.NewValue, CultureInfo.InvariantCulture);
-            OnConflictAction action;
 
-            if (!uniqueText.StartsWith("OnConflict:", StringComparison.OrdinalIgnoreCase))
+            if (uniqueText != null && !uniqueText.StartsWith("OnConflict:", StringComparison.OrdinalIgnoreCase))
                 return string.Empty;
 
-            var actionText = uniqueText.Remove(0, "OnConflict:".Length).Trim();
-            if (!Enum.TryParse(actionText, out action))
+            var actionText = uniqueText?.Remove(0, "OnConflict:".Length).Trim();
+            if (!Enum.TryParse(actionText, out OnConflictAction action))
                 return string.Empty;
 
             if (action == OnConflictAction.None)
@@ -274,7 +403,7 @@ namespace SQLite.CodeFirst.Utility
             string collateFunction;
             string collateCustomFunction;
 
-            if (collateAttributeText.IndexOf(':') > -1)
+            if (collateAttributeText != null && collateAttributeText.IndexOf(':') > -1)
             {
                 collateFunction = collateAttributeText.Substring(0, collateAttributeText.IndexOf(':'));
                 collateCustomFunction = collateAttributeText.Remove(0, collateAttributeText.IndexOf(':') + 1).Trim();
@@ -285,8 +414,7 @@ namespace SQLite.CodeFirst.Utility
                 collateCustomFunction = string.Empty;
             }
 
-            CollationFunction colatteFunctionType;
-            if (!Enum.TryParse(collateFunction, out colatteFunctionType))
+            if (!Enum.TryParse(collateFunction, out CollationFunction colatteFunctionType))
                 return string.Empty;
 
             if (colatteFunctionType == CollationFunction.None)
